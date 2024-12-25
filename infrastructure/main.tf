fe0b57acad8f982aa0beb59b0d0c298f1da41047
ozennou <ozennouamine02@gmail.com>
2024-12-25 15:10:09 +0100

    add promethues port to the security group

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -169,7 +169,7 @@ resource "azurerm_network_security_group" "default" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "9236"
+    destination_port_range     = "9090"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
