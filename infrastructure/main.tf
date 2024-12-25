c87e3056f9aa7ff2b467ef6dad3d9ce6550349e4
ozennou <ozennouamine02@gmail.com>
2024-12-25 16:03:47 +0100

    feat: enable gitlab promethues and add grafana

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -163,13 +163,13 @@ resource "azurerm_network_security_group" "default" {
   }
 
   security_rule {
-    name                       = "Allow-MT"
+    name                       = "Allow-GRAFANA"
     priority                   = 1003
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "9090"
+    destination_port_range     = "3000"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
